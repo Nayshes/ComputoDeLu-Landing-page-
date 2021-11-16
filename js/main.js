bbf6e16e136216db437a518dbd53111b515aa392
Oleg <79206312+Nayshes@users.noreply.github.com>
2021-11-16 13:03:35 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -15,6 +15,40 @@ if (iconMenu) {
     });
 }
 
+//Прокрутка при клике 
+
+const menuLinks = document.querySelectorAll('.meny_link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top;
+
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                MenuBody.classList.remove('_active');
+                MenuBody.classList.remove('menus');
+                mainLogo.classList.remove('logo_prev');
+            }
+
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
+
+
+
 //анимации при скроле
 const animItems = document.querySelectorAll('._anim-items');
 
